459688d6bd8bc54c72a35cc2da8eee64f7b6adba
ken <ynkn0829@gmail.com>
2022-08-21 13:43:52 +0000

    2022-08-21

--- src/vars/sub_a.rs
+++ src/vars/sub_a.rs
@@ -1,3 +1,15 @@
-pub fn func_a() {
-    println!("Here is func_a in sub_a module!!");
+use std::str;
+
+pub fn func_a(str: &str) {
+    println!("{}", str);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn func_a_test() {
+        func_a("Hello, world!");
+    }
 }
