289ca24dbc24b45b4624aa6d4080c13b8d899c21
ken <ynkn0829@gmail.com>
2022-08-21 14:18:09 +0000

    2022-08-21

--- src/vars.rs
+++ src/vars.rs
@@ -1,8 +1,8 @@
-pub mod sub_a;
-mod sub_b;
-
 pub fn run() {
     println!("Here is vars module!!");
-    sub_a::func_a("Hello, world!");
-    sub_b::func_b();
+
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
 }
