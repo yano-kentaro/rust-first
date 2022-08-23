aa2e6c9dda1f9dfdd97b698caa4e4fd613403849
ken <ynkn0829@gmail.com>
2022-08-23 15:53:57 +0000

    構造体とメソッド

--- src/generics.rs
+++ src/generics.rs
@@ -1,9 +1,27 @@
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
 pub fn run() {
     let number_list = vec![34, 50, 25, 100, 65];
     println!("The largest number is {}", largest_i32(number_list));
 
     let char_list = vec!['a', 'b', 'c', 'd', 'e'];
     println!("The largest is {}", largest(char_list));
+
+    let p1 = Point { x: 1, y: 2 };
+    let p2 = Point { x: 1.0, y: 'a' };
+    let p3 = p1.mixup(p2);
+    println!("{} {}", p3.x, p3.y);
 }
 
 fn largest_i32(list: Vec<i32>) -> i32 {
